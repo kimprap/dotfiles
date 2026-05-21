0177d4260e461190ec7457156a6f8d5d0bc117b3
kimp <sura.prap@gmail.com>
2026-05-21 23:47:39 +0700

    fix(zsh): drop broken antidote uv plugin
    uv completion is already provided via eval at end of zshrc.

---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,4 @@
+fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-autosuggestions" )
+source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-syntax-highlighting" )
+source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
